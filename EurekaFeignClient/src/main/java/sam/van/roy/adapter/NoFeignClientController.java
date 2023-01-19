2a91d2e9ea1c83a8026331fdeaa079a01f9f6eae
SamVanRoy <saxo4sam@gmail.com>
2023-01-19 11:20:15 +0100

    replace constructor with RequiredArgsConstructor from Lombok

--- EurekaFeignClient/src/main/java/sam/van/roy/adapter/NoFeignClientController.java
+++ EurekaFeignClient/src/main/java/sam/van/roy/adapter/NoFeignClientController.java
@@ -2,6 +2,7 @@ package sam.van.roy.adapter;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +11,12 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/no-feign")
 public class NoFeignClientController {
 
     private final EurekaClient eurekaClient;
-
-    public NoFeignClientController(EurekaClient eurekaClient) {
-        this.eurekaClient = eurekaClient;
-    }
 
     @GetMapping("/get-hello-world")
     public String getHelloWorld() {
